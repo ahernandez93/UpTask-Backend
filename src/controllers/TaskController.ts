dfa281ad011e6891efc5c97d384eea2e0c1f20d0
AllanHernandez <allan_hc@outlook.com>
2026-03-22 23:32:36 -0600

    hasta video 472

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import Task from "../models/Task";
+
+export class TaskController {
+    static createTask = async (req: Request, res: Response) => {
+        try {
+            const task = new Task(req.body)
+            task.project = req.project._id
+            req.project.tasks.push(task._id)
+            await Promise.allSettled([task.save(), req.project.save()])
+            res.send('Tarea creada correctamente')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
