700dc4a9b09d510931f3037f321d231d6acc969a
Allan <allan_hc@outlook.com>
2026-03-23 18:08:01 -0600

    hasta video 479

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -10,7 +10,74 @@ export class TaskController {
             await Promise.allSettled([task.save(), req.project.save()])
             res.send('Tarea creada correctamente')
         } catch (error) {
-            console.log(error)
+            res.status(500).json('Hubo un error')
+        }
+    }
+
+    static getProjectTasks = async (req: Request, res: Response) => {
+        try {
+            const tasks = await Task.find({ project: req.project._id }).populate('project')
+            res.json(tasks)
+        } catch (error) {
+            res.status(500).json('Hubo un error')
+        }
+    }
+
+    static getTaskById = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params
+            const task = await Task.findById(taskId)
+            if (!task) {
+                const error = new Error('Tarea no encontrada');
+                return res.status(404).json({ error: error.message });
+            }
+
+            if (task.project.toString() !== req.project._id.toString()) {
+                const error = new Error('Accion no valida');
+                return res.status(400).json({ error: error.message });
+            }
+            res.json(task);
+        } catch (error) {
+            res.status(500).json('Hubo un error')
+        }
+    }
+
+    static updateTask = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params
+            const task = await Task.findById(taskId)
+            if (!task) {
+                const error = new Error('Tarea no encontrada');
+                return res.status(404).json({ error: error.message });
+            }
+
+            if (task.project.toString() !== req.project._id.toString()) {
+                const error = new Error('Accion no valida');
+                return res.status(400).json({ error: error.message });
+            }
+            task.name = req.body.name
+            task.description = req.body.description
+            await task.save()
+            res.send('Tarea actualizada correctamente');
+        } catch (error) {
+            res.status(500).json('Hubo un error')
+        }
+    }
+
+    static deleteTask = async (req: Request, res: Response) => {
+        try {
+            const { taskId } = req.params
+            const task = await Task.findById(taskId)
+            if (!task) {
+                const error = new Error('Tarea no encontrada');
+                return res.status(404).json({ error: error.message });
+            }
+
+            req.project.tasks = req.project.tasks.filter(task => task.toString() !== taskId)
+            await Promise.allSettled([task.deleteOne(), req.project.save()])
+            res.send('Tarea eliminada correctamente');
+        } catch (error) {
+            res.status(500).json('Hubo un error')
         }
     }
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -39,12 +39,37 @@ router.delete("/:id",
 );
 
 /** Routes for tasks */
+router.param('projectId', validateProjectExists)
+
 router.post("/:projectId/tasks",
-    validateProjectExists,
     body('name').notEmpty().withMessage('El nombre de la tarea es requerido'),
     body('description').notEmpty().withMessage('La descripcion de la tarea es requerida'),
     handleInputErrors,
     TaskController.createTask
+)
+
+router.get("/:projectId/tasks",
+    TaskController.getProjectTasks
+)
+
+router.get("/:projectId/tasks/:taskId",
+    param('taskId').isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    TaskController.getTaskById
+)
+
+router.put("/:projectId/tasks/:taskId",
+    param('taskId').isMongoId().withMessage('ID no valido'),
+    body('name').notEmpty().withMessage('El nombre de la tarea es requerido'),
+    body('description').notEmpty().withMessage('La descripcion de la tarea es requerida'),
+    handleInputErrors,
+    TaskController.updateTask
+)
+
+router.delete("/:projectId/tasks/:taskId",
+    param('taskId').isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    TaskController.deleteTask
 )
 
 export default router;
