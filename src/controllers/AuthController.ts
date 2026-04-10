692682e6e805359e59f92a473817f073fc81bdee
Allan <allan_hc@outlook.com>
2026-04-10 15:51:06 -0600

    Implement request confirmation code feature and update email template

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -36,7 +36,7 @@ export class AuthController {
             })
 
             await Promise.allSettled([user.save(), token.save()])
-            res.send('Cuenta creada, revisa tu email para confirmarla ')
+            res.send('Cuenta creada, revisa tu email para confirmarla')
         } catch (error) {
             res.status(500).json({ error: 'Hubo un error' })
         }
@@ -98,6 +98,41 @@ export class AuthController {
 
             res.send('Autenticado...')
 
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' })
+        }
+    }
+
+    static requestConfirmationCode = async (req: Request, res: Response) => {
+        try {
+            const { email } = req.body
+
+            //Usuario existe
+            const user = await User.findOne({ email })
+            if (!user) {
+                const error = new Error('El usuario no esta registrado')
+                return res.status(404).json({ error: error.message })
+            }
+
+            if (user.confirmed) {
+                const error = new Error('El usuario ya esta confirmado')
+                return res.status(403).json({ error: error.message })
+            }
+
+            //Generar el token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user._id
+
+            //Enviar el email
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.name,
+                token: token.token
+            })
+
+            await token.save()
+            res.send('Se envión un nuevo token a tu email')
         } catch (error) {
             res.status(500).json({ error: 'Hubo un error' })
         }
