adc74299776c91b354d48bdc6c8373df485b18aa
AllanHernandez <allan_hc@outlook.com>
2026-04-12 03:33:59 -0600

    Se implemento JSON web token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,6 +4,7 @@ import { checkPassword, hashPassword } from '../utils/auth'
 import Token from '../models/Token'
 import { generateToken } from '../utils/token'
 import { AuthEmail } from '../emails/AuthEmail'
+import { generateJWT } from '../utils/jwt'
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
@@ -96,7 +97,8 @@ export class AuthController {
                 return res.status(401).json({ error: error.message })
             }
 
-            res.send('Autenticado...')
+            const token = generateJWT({ id: user._id })
+            res.json({ token })
 
         } catch (error) {
             res.status(500).json({ error: 'Hubo un error' })
