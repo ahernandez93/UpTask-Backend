cd234d3edf84cdcbfbc3d1e106a9926142e87d7e
Allan <allan_hc@outlook.com>
2026-04-21 17:48:58 -0600

    hasta video 634

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -66,4 +66,26 @@ router.post('/update-password/:token',
 
 router.get('/user', authenticate, AuthController.user)
 
+/** Profile */
+router.put('/profile',
+    authenticate,
+    body('name').notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('email').isEmail().withMessage('E-mail no valido'),
+    handleInputErrors,
+    AuthController.updateProfile
+)
+
+router.post('/update-password',
+    authenticate,
+    body('current_password').notEmpty().withMessage('El password actual no puede ir vacio'),
+    body('password').isLength({ min: 8 }).withMessage('El password es muy corto, minimo 8 caracteres'),
+    body('password_confirmation').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Los password no son iguales')
+        }
+        return true
+    }),
+    handleInputErrors,
+    AuthController.updateCurrentUserPassword)
+
 export default router
